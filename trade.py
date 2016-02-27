0a38818ade1a70cd29a9b06d5bbca3cc6bed7cb6
kushner <kobikson@gmail.com>
2016-02-27 15:33:26 +0100

    BUY/SELL BOND

--- trade.py
+++ trade.py
@@ -14,17 +14,27 @@ def trade():
     bond_request = trade_BOND(50000)
     if bond_request:
         price, amount = bond_request[0], bond_request[1]
-        return price, amount
+        return "BOND", price, amount
 
 trade.counter = 0
 
 
 def trade_BOND(price_limit):
     bond = SECURITIES['BOND']
-    bond_offer = OFFERS['BOND']
 
     value = 1000
-    if bond.our_amount > 0 and bond_offer:
-        price = 995
-        amount = price_limit / 995
+    if abs(bond.our_amount) < 10 or abs(bond.our_amount_waiting) < 10:
+        raise AssertionError('TO LOW VALUE')
+    if bond.our_amount_waiting == 0:
+        if bond.our_amount > 0:
+            # sell
+            price = 1001
+            amount = bond.our_amount
+            amount = -amount
+        elif bond.our_amount == 0:
+            # buy
+            price = 999
+            amount = price_limit / price
+        else:
+            AssertionError('value < 0 ?')
     return price, amount
